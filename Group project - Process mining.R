940b55fa2f53f54e54a8d32f2a284f86b1a4683d
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2024-04-17 19:19:05 +0530

    done minor changes in R script

--- Group project - Process mining.R
+++ Group project - Process mining.R
@@ -16,4 +16,52 @@ class(data$Case_ID)  #check the class of a particular variable
 unique(data$Case_ID)  #what can we found inside a particular variable
 levels(data$Case_ID)  #check the levels of any variable
 
+#installing the required packages for process mining
+install.packages("bupaR")
+install.packages("processmapR")
+install.packages("eventdataR")
+
+#loading the required libraries for process mining
+library(bupaR)
+library(eventdataR)
+library(processmapR)
+library(edeaR)
+library(dplyr)
+library(tidyverse)
+
+data <- data %>%
+  arrange(Case_ID,Opened_At)%>%
+  group_by(Case_ID)%>%
+  mutate(activity_instance_id = paste(Case_ID,Enrollment_Status, row_number(), sep="_"))%>%
+  ungroup()
+
+dim(data)
+
+head(data)
+
+class(data)
+
+mapping(data) #there is an error after this code line
+
+summary(data)
+
+eventlog <- read_csv("D:\\My Projects 1\\R-Coursework-V2.0\\Student_Enrollment_Event_Log.csv")
+
+# Example preprocessing steps
+eventlog <- filter_trace_starts(eventlog, condition = "activity %in% c('Start', 'TaskA', 'TaskB')")
+eventlog <- adjust_time_zone(eventlog, new_zone = "GMT")
+
+process_map <- process_map(eventlog)
+
+plot(process_map)
+
+
+# Convert dataframe to event log object
+eventlog <- as_eventlog(eventlog_df, case_id = "CaseID", activity_id = "Activity", timestamp = "Timestamp")
+
+# Create process map
+process_map <- process_map(eventlog)
+
+# Visualize process map
+plot(process_map)
 
