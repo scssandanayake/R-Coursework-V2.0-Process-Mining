c2e20b58e6c282f3a860deb267c41ca7d157f6da
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2024-04-23 13:17:04 +0530

    minor changes

--- heuristic minor algosrithm - process mining.R
+++ heuristic minor algosrithm - process mining.R
@@ -3,7 +3,6 @@
 library(renv)
 renv::init()
 
-#NN
 #NNremotes::install_github("bupaverse/pm4py@dev")
 #pm4py::install_pm4py()
 #install.packages("reticulate")
@@ -72,6 +71,7 @@ library(heuristicsmineR)
 library(DiagrammeRsvg)
 library(magrittr)
 library(igraph)
+library(petrinetR)
 
 #get bupar packages
 help(package = "bupaR")
@@ -81,77 +81,6 @@ cn <- causal_net(event_log,threshold = 0.95)
 pn <- as.petrinet(cn)
 #drawing the petrinet
 petrinetR::render_PN(pn)
-
-
-
-
-
-
-######## FIND POTIMAL PATH #########
-places <- pn$places$id
-transitions <- pn$transitions$id
-
-# Create igraph vertices for places and transitions
-vertices <- c(places, transitions)
-
-# Create igraph edges from preset and postset of transitions
-edges <- c()
-for (t in transitions) {
-  pre <- pn$transitions$preset[t]
-  post <- pn$transitions$postset[t]
-  edges <- c(edges, paste0(pre, "-", t), paste0(t, "-", post))
-}
-
-# Extract unique vertices from edges
-unique_vertices <- unique(unlist(strsplit(edges, "-")))
-
-# Check if all vertices are present in the vertex list
-missing_vertices <- setdiff(unique_vertices, vertices)
-if (length(missing_vertices) > 0) {
-  stop("Some vertices in the edge list are not listed in the vertex data.")
-}
-
-# Create igraph graph
-graph <- graph_from_data_frame(data.frame(from = substr(edges, 1, 1), to = substr(edges, 3, 3)), vertices = vertices, directed = TRUE)
-
-# Get all possible paths in the Petri net
-all_paths <- all_simple_paths(graph, from = V(graph)[degree(graph, mode = "in") == 0], to = V(graph)[degree(graph, mode = "out") == 0])
-
-# Print all possible paths
-print(all_paths)
-
-# Get the optimal path based on a certain criterion (e.g., shortest path)
-optimal_path <- shortest_paths(graph, from = V(graph)[degree(graph, mode = "in") == 0], to = V(graph)[degree(graph, mode = "out") == 0], mode = "out")$vpath
-
-# Print the optimal path
-print(optimal_path)
-
-# Get the optimal path based on a certain criterion (e.g., shortest path)
-shortest_paths_result <- shortest_paths(graph, from = V(graph)[degree(graph, mode = "in") == 0], to = V(graph)[degree(graph, mode = "out") == 0], mode = "out")
-
-# Extract and format the optimal path vertices
-optimal_path_vertices <- lapply(shortest_paths_result$vpath, function(path) {
-  if (length(path) > 0) {
-    path_names <- names(path)
-    vertex_names <- V(graph)$name[path_names]
-    paste(vertex_names, collapse = " -> ")
-  } else {
-    "No valid path found"
-  }
-})
-
-# Filter out paths where no valid path was found
-valid_optimal_paths <- optimal_path_vertices[sapply(optimal_path_vertices, function(path) path != "No valid path found")]
-
-# Print the valid optimal paths
-print(valid_optimal_paths)
-
-
-
-
-
-
-
 
 #generate causal net
 causal_net(event_log,threshold = 0.95) %>%
